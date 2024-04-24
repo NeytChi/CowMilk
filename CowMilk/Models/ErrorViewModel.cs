0d93fd8fcda80eebb88d1f793024ccbde522d42e
neyto <neyto@MYKYTACHYSTYI>
2024-04-24 10:14:42 +0300

    Add project files.

---
+++ CowMilk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CowMilk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
